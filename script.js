14ca9c9f1833f63be547aa0ea91c0fef1d6397ac
danthekidd <daniassothekidd@gmail.com>
2025-11-27 21:03:28 +0000

    new commit

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const { createFFmpeg, fetchFile } = FFmpeg;
 
 const ffmpeg = createFFmpeg({
-    log: true,
-    corePath: "https://unpkg.com/@ffmpeg/core@0.11.0/dist/ffmpeg-core.js"
+    log: true
 });
 
 ffmpeg.load();
